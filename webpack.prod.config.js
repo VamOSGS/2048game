1b517f98e31c0fde6a940684710e8c05e38c793e
Gegham Samvelyan <vamosgs2001@gmail.com>
2017-12-08 00:15:18 +0400

    build fix

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -41,9 +41,9 @@ module.exports = {
   plugins: [
     extractLess,
     new HtmlWebpackPlugin({
-      title: 'game',
+      title: 'asd',
+      template: './app/template.html',
       inject: 'body',
-      template: './app/template.html'
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
